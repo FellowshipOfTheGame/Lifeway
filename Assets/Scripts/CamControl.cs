3765a41c8ded5a2764799aca8fbf6f1cc070a20a
Unknown <will.leidan@gmail.com>
2018-10-31 09:31:10 -0300

    Camera Control

---
+++ Assets/Scripts/CamControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour {
+
+	public Transform player;
+    public float delay, offset;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.rotation = Quaternion.Lerp(this.transform.rotation, player.transform.rotation, delay);
+		this.transform.position = Vector3.Lerp(this.transform.position, player.transform.position - Vector3.forward * 10.0f + player.up * offset, delay);
+		
+	}
+}
