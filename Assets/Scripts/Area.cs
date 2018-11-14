9d6798ea5ef00d8af59396f9a23eb4162e92a722
Unknown <will.leidan@gmail.com>
2018-11-14 00:18:28 -0200

    Area Division

---
+++ Assets/Scripts/Area.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Area : MonoBehaviour {
+
+	public List<GameObject> players;
+	public GameObject zone;
+
+	// Use this for initialization
+	void Start () {
+		players = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void addPlayer(GameObject p){
+		if(players.Count == 0) zone.SetActive(true);
+		players.Add(p);
+	}
+
+	public void removePlayer(GameObject p){
+		players.Remove(p);
+		if(players.Count == 0) zone.SetActive(false);
+	}
+}
