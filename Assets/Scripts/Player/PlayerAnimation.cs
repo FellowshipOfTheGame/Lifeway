f2c9c343b1545f801566dff1677fddcdffb6d4f2
Michelle Wingter <mwingter@gmail.com>
2018-11-20 23:12:51 -0200

    Ship Collision

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+	[SerializeField]
+	private Animator anim;
+	[SerializeField]
+	private ParticleSystem particles;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Animador(float vel){
+		anim.SetFloat("velocity_tale", vel);
+	}
+
+	public void Die(){
+		anim.enabled = false;
+		DeactivateParticles();
+	}
+
+	public void ActivateParticles(){
+		particles.Play();
+		ParticleSystem.MainModule aux = particles.main;
+		aux.loop = true;
+	}
+
+	public void DeactivateParticles(){
+		ParticleSystem.MainModule aux = particles.main;
+		particles.Stop();
+		aux.loop = false;
+	}
+}
