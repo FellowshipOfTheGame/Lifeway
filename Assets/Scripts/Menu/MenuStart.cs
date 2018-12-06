28a864ae5c9883a6cdc037f8883cbd18da0dd082
Michelle Wingter <mwingter@gmail.com>
2018-12-06 21:57:17 -0200

    main menu

---
+++ Assets/Scripts/Menu/MenuStart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuStart : MonoBehaviour {
+
+	public void changeScene(string sceneName){
+		//Application.LoadLevel(sceneName);
+		SceneManager.LoadScene(sceneName);
+	}
+}
