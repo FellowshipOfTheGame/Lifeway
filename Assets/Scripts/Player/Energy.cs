34301600a5fc5d06de69a8dc3a701f0e33e48829
Michelle Wingter <mwingter@gmail.com>
2018-12-23 23:21:35 -0200

    Revert "dando merge de michelle com merge2"
    This reverts commit 05e0c229560952c516a586cf04a89a8fb800940d.

---
+++ Assets/Scripts/Player/Energy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour {
+
+	private float currentEnergy;
+	[SerializeField]
+	private float maxEnergy;
+	[SerializeField]
+	private float deltaEnergy;
+	public Image eBar;
+	
+	[SerializeField]
+	private Ship ship;
+	public GameObject loserText, backButton;
+
+	// Use this for initialization
+	void Start () {
+		currentEnergy = maxEnergy;
+		loserText.SetActive(false);
+		backButton.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(ship.motor){
+			DecreaseEnergy(deltaEnergy * Time.deltaTime);
+		}
+		//Debug.Log(currentEnergy);
+		eBar.fillAmount = currentEnergy/maxEnergy;
+
+	}
+	public void AddEnergy(float e){
+		currentEnergy += e;
+		if (currentEnergy > maxEnergy){
+			currentEnergy = maxEnergy;
+		}
+	}
+
+	public void DecreaseEnergy(float e){
+		currentEnergy -= e;
+		if(currentEnergy <= 0){
+			currentEnergy = 0;
+			loserText.SetActive(true);
+			backButton.SetActive(true);
+			Debug.Log("FALICEU");
+			ship.MotorDie();
+		}
+	}
+}
